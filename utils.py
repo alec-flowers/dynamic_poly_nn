df3af8511ad21caf5d83a8b5deef030c61aaaddb
Alec Flowers <alec.flowers00@gmail.com>
2022-03-12 20:12:46 +0100

    [utils][chore] create folders if they don't exist

--- utils.py
+++ utils.py
@@ -1,4 +1,17 @@
 import pathlib
+import os
+
+
+def path_exist(path):
+    isExist = os.path.exists(path)
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(path)
+        print("The new directory is created!")
+
 
 REPO_ROOT = pathlib.Path(__file__).absolute().parents[0].resolve()
 assert (REPO_ROOT.exists())
+
+MODEL_PATH = (REPO_ROOT / "models").absolute().resolve()
+path_exist(MODEL_PATH)
