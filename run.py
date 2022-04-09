38e2c8dcb0d446a1391acc63323fb74b61566269
Alec Flowers <alec.flowers00@gmail.com>
2022-04-09 17:57:33 +0200

    [run][refact] change to yaml files, other small changes

--- run.py
+++ run.py
@@ -1,138 +1,142 @@
+import argparse
+import importlib
+import random
 import torch
 import os
 from torch import optim
-from torchvision import transforms
+import yaml
 from torch.utils.tensorboard import SummaryWriter
 import datetime
-from utils import MODEL_PATH, path_exist, count_parameters, load_checkpoint, WRITER_PATH
+from utils import path_exist, count_parameters, load_checkpoint, REPO_ROOT, CONFIGS_PATH, save_checkpoint, load_model, set_seed, init_weights
 import time
 
-from load_data import load_dataset, only_use_certain_class, load_trainloader, OneClassDataset, FewClassDataset, GreyToColorDataset
-import nets
-from runner import train, test, train_profile
+from load_data import load_dataset, load_trainloader
+from networks import nets
+from runner import train, test
+
+# TODO - Learning Rate Milestones
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="run networks")
+    parser.add_argument("--config_name", type=str,
+                        required=True, default='yml',
+                        help="Name of YAML config file.")
+
+    return parser.parse_args()
+
+def run(args):
+    curdir = os.path.abspath(os.path.curdir)
+    print(f"Current Running Directory: {curdir}")
+    print(f"Repo Root: {REPO_ROOT}")
+
+    try:
+        with open(os.path.join(CONFIGS_PATH, args.config_name), 'r') as file:
+            config = yaml.safe_load(file)
+    except Exception as e:
+        print('Error reading the config file')
+
+    if config['seed'] is None:
+        seed = random.randint(1, 1000)
+    else:
+        seed = config['seed']
+    set_seed(seed)
+
+    # Create all paths we will need
+    custom_save = config["custom_save"]
+    path_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    folders_for_save = f"logs/{config['dataset']['name']}/{config['model']['name']}_{custom_save}_{path_time}"
+    savepath = f"{curdir}/{folders_for_save}"
+    path_exist(savepath)
+
+    with open(os.path.join(savepath, f"r_{args.config_name}"), 'w') as file:
+        yaml.dump(
+            config,
+            file,
+            default_flow_style=False,
+        )
+
+    # Load Data in particular way
+    train_dataset, test_dataset = load_dataset(**config["dataset"])
+
+    # Initialize dataloaders
+    train_loader, valid_loader = load_trainloader(
+        train_dataset,
+        seed=seed,
+        **config["dataloader"]
+    )
+
+    #TODO may need to change this with RESNET
+    print(f"Length of iters per epoch: {len(train_loader)}. Length of valid batches: {len(valid_loader)}. Size of img: {train_loader.dataset[0][0].shape}")
+    sample_shape = train_loader.dataset[0][0].shape
+    assert(sample_shape[1] == sample_shape[2]), "Image is not square, but only use one side"
+    image_size = sample_shape[1]
+    num_classes = len(train_loader.dataset.classes)
+    channels_in = sample_shape[0]
+
+    # create the model.
+    net = load_model(
+        config['model'],
+        image_size=image_size, # this parameter doesn't matter for Resnet
+        num_classes=num_classes,
+        channels_in=channels_in)
+
+    net.init = config['model']['args'].get('init', None)
+    init_weights(net)
+
+    num_params = count_parameters(net)
+    deg = config['model']['args'].get('n_degree', config['model']['args'].get('num_blocks', None))
+    print(f"Degree: {deg} Num Parameters: {num_params}")
+
+    # `define the optimizer.
+    decay = config['training_info'].get('weight_dec', 5e-4)
+    opt = optim.SGD(net.parameters(), lr=config['training_info']['lr'],
+                    momentum=0.9, weight_decay=decay)
+    cuda = torch.cuda.is_available()
+    device = torch.device('cuda' if cuda else 'cpu')
+    print(f"Running on: {device}")
+
+    start_epoch = -1
+    if config['checkpoint']:
+        checkpoint = load_checkpoint(net, config['checkpoint'], opt)
+        start_epoch = checkpoint['epoch']
+
+    if device == 'cuda':
+        print(f"Num Devices: {torch.cuda.device_count()}")
+        dev = torch.cuda.current_device()
+        print(f"Device Name: {torch.cuda.get_device_name(dev)}")
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    net.to(device)
+
+    mil = config['training_info'].get('lr_milestones', [40, 60, 80, 100])
+    gamma = config['training_info'].get('lr_gamma', 0.1)
+    # TODO May be something finicky here when loading a model and training.
+    scheduler = optim.lr_scheduler.MultiStepLR(opt, milestones=mil,
+                                               gamma=gamma, last_epoch=start_epoch)
+    writer = SummaryWriter(savepath)
+
+    start_time = time.time()
+    old_acc = 0
+    for epoch in range(start_epoch+1, config['training_info']['epochs']+1):
+        train(net, train_loader, opt, criterion, epoch, device, writer, display_interval=config['training_info']['display_interval'])
+
+        if epoch % config['training_info']['save_every_epoch'] == 0 and epoch > 0:
+            elapsed_time = time.time()-start_time
+            save_checkpoint(net, opt, epoch, elapsed_time, savepath, f"latest_e{epoch}_t{elapsed_time:.0f}")
+
+        if epoch % config['training_info']['test_every_epoch'] == 0:
+            acc = test(net, valid_loader, criterion, epoch, device, writer)
+            if acc > old_acc:
+                old_acc = acc
+                elapsed_time = time.time() - start_time
+                save_checkpoint(net, opt, epoch, elapsed_time, savepath, f"best_model")
+
+        scheduler.step()
+
+    writer.flush()
+    total_time = time.time() - start_time
+    print(f"Total Training Time: {total_time:.1f} seconds")
 
 
 if __name__ == '__main__':
-
-    CUSTOM_SAVE = ""
-    save = True
-    DATASETS = ['MNIST', "FashionMNIST", "CIFAR10"]
-    chosen_dataset = DATASETS[2]
-    checkpoint = None
-    confusion_matrix = False
-    subset = False
-    num_workers = 4
-    apply_manipulation = None
-    ind_to_keep = None
-    binary_class = None
-
-    # Hyperparameters
-    net_name = "CCP"
-    lr = 0.001
-    epochs_list = [100]
-    batch_size = 64
-    n_degree_list = [2]
-    hidden_size = 1000  # p = 2n + d
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-
-
-# ============================== RUN ============================== #
-    for i in range(len(epochs_list)):
-        epochs = epochs_list[i]
-        n_degree = n_degree_list[i]
-
-        path_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        folders = f"{chosen_dataset}/{n_degree}"
-        filename = f"{CUSTOM_SAVE}_e{epochs}_{path_time}"
-        path_exist(f"{WRITER_PATH}/{folders}", f"{MODEL_PATH}/{folders}")
-        W_PATH = f"{WRITER_PATH}/{folders}/{filename}"
-        M_PATH = f"{MODEL_PATH}/{folders}/{filename}.ckpt"
-
-        # Load Data in particular way
-        train_dataset, test_dataset = load_dataset(
-            chosen_dataset,
-            transform,
-            apply_manipulation=apply_manipulation,
-            binary_class=binary_class,
-            ind_to_keep=ind_to_keep,
-            color=None
-        )
-        # Initialize dataloaders
-        train_loader, valid_loader = load_trainloader(
-            train_dataset,
-            batch_size=batch_size,
-            num_workers=num_workers,
-            shuffle=True,
-            valid_ratio=0.2,
-            seed=0,
-            subset=subset
-        )
-
-        print(f"Dataset: {type(train_loader.dataset)}")
-        sample_shape = train_loader.dataset[0][0].shape
-        #check image is square since using only 1 side of it for the shape
-        assert(sample_shape[1] == sample_shape[2])
-        image_size = sample_shape[1]
-        n_classes = len(train_loader.dataset.classes)
-        channels_in = sample_shape[0]
-
-        # create the model.
-        net = getattr(nets, net_name)(
-            hidden_size=hidden_size,
-            image_size=image_size,
-            n_classes=n_classes,
-            channels_in=channels_in,
-            n_degree=n_degree
-        )
-        net.apply(net.weights_init)
-        num_params = count_parameters(net)
-        print(f"Degree: {n_degree} Num Parameters: {count_parameters(net)}")
-
-        # `define the optimizer.
-        opt = optim.SGD(net.parameters(), lr=lr)
-        cuda = torch.cuda.is_available()
-        device = torch.device('cuda' if cuda else 'cpu')
-        print(f"Running on: {device}")
-
-        if checkpoint:
-            load_checkpoint(net, checkpoint, opt)
-
-        if device == 'cuda':
-            print(f"Num Devices: {torch.cuda.device_count()}")
-            dev = torch.cuda.current_device()
-            print(f"Device Name: {torch.cuda.get_device_name(dev)}")
-        criterion = torch.nn.CrossEntropyLoss().to(device)
-
-        writer = SummaryWriter(W_PATH)
-
-        start_time = time.time()
-        for epoch in range(epochs):
-            train(net, train_loader, opt, criterion, epoch, device, writer, confusion_matrix)
-            if epoch % 5 == 0:
-                test(net, valid_loader, criterion, epoch, device, writer)
-        writer.flush()
-        total_time = time.time() - start_time
-        print(f"Total Training Time: {total_time:.1f} seconds")
-
-        if save:
-            print("Saving Model")
-            torch.save(
-                {
-                    'model_state_dict': net.state_dict(),
-                    'optimizer_state_dict': opt.state_dict(),
-                    'chosen_dataset': chosen_dataset,
-                    'subset': subset,
-                    "num_workers": num_workers,
-                    "apply_manipulation": apply_manipulation,
-                    "ind_to_keep": ind_to_keep,
-                    "binary_class": binary_class,
-                    "lr": lr,
-                    "epochs": epochs,
-                    "batch_size": batch_size,
-                    "n_degree": n_degree,
-                    "transform": transform,
-                    "hidden_size": hidden_size,
-                    "total_time": total_time,
-                    "net_name": net_name,
-                    "num_params": num_params
-                 }, M_PATH)
+    args = parse_args()
+    run(args)
