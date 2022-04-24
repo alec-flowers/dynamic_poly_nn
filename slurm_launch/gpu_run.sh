b59ab8c7d02a48016c18ae5ff04c0184bdcfd6a5
Alec Flowers <alec.flowers00@gmail.com>
2022-04-24 17:45:51 +0200

    last bits

---
+++ slurm_launch/gpu_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=poly_run
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu_free
+#SBATCH --gres=gpu:1
+#SBATCH --chdir /scratch/izar/flowers/
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --time 01:00:00
+
+echo STARTING AT `date`
+
+module purge
+module load gcc/8.4.0 python/3.7.7
+source /home/flowers/venvs/dpolynn_venv/bin/activate
+
+python3 /home/flowers/dynamic_poly_nn/run.py --config_name resnet_default.yml
+
+deactivate
+
+echo FINISHED at `date`
